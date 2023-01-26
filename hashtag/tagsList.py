b244e77647bf1c333ad4ec579b48b938c694ac84
JJongSSu <112380397+JJongSSu@users.noreply.github.com>
2023-01-26 16:58:23 +0900

    first commit

---
+++ hashtag/tagsList.py
@@ -0,0 +1,22 @@
+Tags = {
+	"boksundoga": [
+		'샴페인', '달달', '깔끔하다', '와인', '시큼하다', '스파클링', '달다', '신맛', '상큼', '청량감', '고급스럽다', '달콤하다', '저렴하다', '가볍다', '단맛',
+        '요구르트', '진하다', '산미', '사이다', '청량하다'
+	],
+
+	"maeul": [
+		"android", "smartPhone", "androidStudio", "studio", "java", "mobile",
+		"mobileApplication", "mobileDeveloper", "google", "googleDevelopers", 
+		"androidDevelopent", "UX", "UI"
+	],
+
+	"web": [
+		"web", "webDevelopment", "frontEnd", "backEnd", "website", "webapp",
+		"cloud", "hosting", "webDeveloper", "database", "html", "css", "javascript",
+		"html5", "js", "webdesign", "webdesigner", "internet", "IT", "data", "UX", "UI"
+	],
+
+	"raspberrypi": [
+		"raspberrypi", "pi", "hardware", "microcontroller", "IoT", "automation"
+	]
+}
